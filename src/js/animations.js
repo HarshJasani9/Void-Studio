234f9a94a86c80e9cd3f139dc8c1ec5744ea4740
HarshJasani9 <harshjasani37@gmail.com>
2026-06-12 13:26:59 +0530

    chore: remove deprecated initHeroAnimations from animations.js

--- src/js/animations.js
+++ src/js/animations.js
@@ -8,39 +8,6 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger';
 gsap.registerPlugin(ScrollTrigger);
 
 /**
- * Hero entrance sequence
- */
-export function initHeroAnimations() {
-  const tl = gsap.timeline({ delay: 1.6 });
-
-  tl.from('.hero__label', {
-    y: 30,
-    autoAlpha: 0,
-    duration: 0.8,
-    ease: 'power3.out',
-  })
-  .from('.hero__title', {
-    y: 60,
-    autoAlpha: 0,
-    duration: 1,
-    ease: 'power3.out',
-  }, '-=0.5')
-  .from('.hero__subtitle', {
-    y: 20,
-    autoAlpha: 0,
-    duration: 0.6,
-    ease: 'power3.out',
-  }, '-=0.4')
-  .from('.hero__scroll', {
-    autoAlpha: 0,
-    duration: 0.6,
-    ease: 'power2.out',
-  }, '-=0.2');
-
-  return tl;
-}
-
-/**
  * Infinite marquee scroll
  */
 export function initMarquee() {
